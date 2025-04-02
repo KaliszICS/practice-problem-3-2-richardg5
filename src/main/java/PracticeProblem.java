b445d1e4269dd69b313c724a41a432477ca34a97
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-04-02 18:48:45 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,13 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+	public static int towerOfHanoi(int num) {
+		if (num < 3) {
+			return -1;
+		} else if (num == 3) {
+			return 7;
+		}
 
-	public static void q2() {
-		//Write question 2 code here
+		return towerOfHanoi(num - 1) * 2 + 1;
 	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
